8c5ba46f8e10f61bad9fc31c54efb9f97e534c33
VY3315 <91747107+VY3315@users.noreply.github.com>
2022-11-21 17:50:17 +0530

    SE

---
+++ SE/Module 3.1/Practical-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+ int main()  
+  {
+     printf("Name   : Vyas Yagnesh\n"); 
+     printf("DOB    : Nov 15, 2001\n"); 
+     printf("Mobile : 00-0000000000\n");
+	 printf("Address : ABCD....\n"); 
+     return(0); 
+  }
