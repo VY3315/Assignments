9fa5a1cd5f4ec9a1398d03a47d711004b8c37456
Vyas Yagnesh <91747107+VY3315@users.noreply.github.com>
2023-12-02 15:41:36 +0530

    Add files via upload

---
+++ MVC .NET/Module-3/Practicales/Demo/Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Value of A:");
+            int a= int.Parse(Console.ReadLine());
+
+            Console.Write("Value of B:");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Adiition is : {0}",a+b);
+            Console.WriteLine("Substraction is : {0}",a-b);
+            Console.WriteLine("Division is : {0}", a / b);
+            Console.WriteLine("Multipication is : {0}", a * b);
+            Console.WriteLine("Modulus is : {0}", a % b);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
